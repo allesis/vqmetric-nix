18f95c4254eefc738c65d1c2e823151da462cd26
Sean Allesina-McGrory <allesis@wwu.edu>
2025-12-09 09:18:37 -0800

    Add nix flake
    Signed-off-by: Sean Allesina-McGrory <allesis@wwu.edu>

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "NixOS derivation of vqmetrics";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+  # TODO: Does not work on x86_64-darwin due to incomprehensible linking errors
+  # will fix at some point, maybe
+    supportedSystems = ["x86_64-linux"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+          inherit system;
+        });
+  in {
+    packages = forEachSupportedSystem ({
+      pkgs,
+      system,
+      ...
+    }: {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "vqmetrics";
+        version = "1.0";
+        src = self;
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.opencv4
+        ];
+
+        buildInputs = [
+          pkgs.pkg-config
+          pkgs.opencv4
+        ];
+
+        buildPhase = ''
+          make clean
+          make
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp -v ./vqtool $out/bin/
+        '';
+
+        meta = with pkgs.lib; {
+          description = "Command‑line tool for VQMetrics";
+          homepage = "https://github.com/allesis/vqmetric";
+          license = licenses.mit;
+          maintainers = with maintainers; [Sean Allesina-McGrory];
+          platforms = platforms.unix;
+        };
+      };
+    });
+
+    devShells = forEachSupportedSystem ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.gcc pkgs.make];
+      };
+    });
+  };
+}
